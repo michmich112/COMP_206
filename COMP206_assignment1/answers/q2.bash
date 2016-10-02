b4e6572b5e49b3a2e9e9d60375a98fe86bf9a818
Michel CANTACUZENE <mcanta5@ds2.CS.McGill.CA>
2016-10-02 13:03:11 -0400

    Testing and file correction needs to be done

--- COMP206_assignment1/answers/q2.bash
+++ COMP206_assignment1/answers/q2.bash
@@ -4,6 +4,7 @@
 #Author:	Michel Cantacuzene
 #Version:	0.1
 #Name:		q2.bash
+#Description:	Prevides optimal time waste by displaying N images (N being a paramater) at a time to the user and cycling endlessly.
 #
 
 #define the numebr of images to be opened:
@@ -15,13 +16,9 @@ FILES="blue_whale.jpg crocodile.jpg elephant.jpg giraffe.jpg hippopotamus.jpg hu
 #we kill all the preveously opened eog instances (instances that were opened by the program)
 #We first verify if there are eog processes open
 PROC=`ps | grep eog`
-echo $PROC
 if [ -n "$PROC" ]
 then
-	echo "isntance found"
 	killall eog
-else
-	echo "no instance found"
 fi
 
 #function to open a new instance of eog with a different animal
@@ -29,10 +26,8 @@ function openNew()
 {
 	NAME=`echo $FILES | cut -d' ' -f1`
 	FILES="`echo $FILES | cut -d' ' -f2-` ${NAME}"
-	echo $FILES
-	eog "$NAME" -n &
+	eog "$NAME" -n & #correct for accessing in the jpgs directory
 	INSTANCES=`ps | grep eog | wc -l`
-	echo instances = $INSTANCES #echo here
 }	
 
 #initial operation
@@ -41,8 +36,6 @@ function initiate()
 	if [ -z `ps | grep eog` ] 
 	then
 		INSTANCES=`ps | grep eog | wc -l`
-		echo $INSTANCES #echo here
-		echo $NUM #echo here
 		if [ $INSTANCES -lt  $NUM ]
 		then
 			while [ $INSTANCES -lt  $NUM ]
@@ -61,7 +54,6 @@ function main()
 		INSTANCES=`ps | grep eog | wc -l`
 		if [ $INSTANCES -lt $NUM ]
 		then
-			sleep 2
 			openNew
 		fi
 	done
