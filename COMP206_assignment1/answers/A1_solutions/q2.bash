44d6381ff0b4954f7f4dabea9e8483dfbf5eaef8
Michel Cantacuzene <Michel@dhcp236253.rez.mcgill.ca>
2016-10-18 18:55:11 -0400

    A1 solutions updated

---
+++ COMP206_assignment1/answers/A1_solutions/q2.bash
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+#
+#Author:	Michel Cantacuzene
+#Version:	0.1
+#Name:		q2.bash
+#Description:	Provides optimal time waste by displaying N images (N being a paramater) at a time to the user and cycling endlessly.
+#
+
+#define the numebr of images to be opened:
+NUM=$1
+
+#define the files we can use
+FILES="blue_whale.jpg crocodile.jpg elephant.jpg giraffe.jpg hippopotamus.jpg humpback_whale.jpg ostrich.jpg sea_turtle.jpg"
+
+#function to open a new instance of eog with a different animal
+function openNew()
+{
+	NAME=`echo $FILES | cut -d' ' -f1`
+	FILES="`echo $FILES | cut -d' ' -f2-` ${NAME}"
+	eog "$NAME" -n & #correct for accessing in the jpgs directory
+	INSTANCES=`ps | grep eog | wc -l`
+}	
+
+#initial operation
+function initiate()
+{
+	if [ -z `ps | grep eog` ] 
+	then
+		INSTANCES=`ps | grep eog | wc -l`
+		if [ $INSTANCES -lt  $NUM ]
+		then
+			while [ $INSTANCES -lt  $NUM ]
+			do
+				openNew
+			done
+		fi
+	fi
+}
+
+#main function
+function main()
+{
+	
+	#we kill all the preveously opened eog instances (instances that were opened by the program)
+	#We first verify if there are eog processes open
+	PROC=`ps | grep eog`
+	if [ -n "$PROC" ]
+	then
+		killall eog
+	fi
+	
+	case $NUM in
+		''|*[!0-9]*)
+			echo "Please input a positive integer to run the program" ;;
+		*)
+			initiate
+			while [ 1 ]
+			do
+				INSTANCES=`ps | grep eog | wc -l`
+				if [ $INSTANCES -lt $NUM ]
+				then
+					openNew
+				fi
+			done
+	esac
+}
+
+#program
+main
---
+++ COMP206_assignment1/answers/A1_solutions/q3.bash
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+#
+#Author:	Michel Cantacuzene
+#Version:	0.1
+#Name:		q3.bash
+#Description:	Sorts the given animals using 3 categories: Name, Wheight, Length.
+#
+
+
+ANIMALS="blue_whale crocodile elephant giraffe hippopotamus humpback_whale ostrich sea_turtle"
+
+function picmake() #function that creates the result.jpg image
+{
+	#first parameter is the filename
+	filename=$1
+	animal=`head -n 1 $filename | tail -n 1`
+	file=$animal".jpg"
+	cp $file result.jpg
+	for i in 2 3 4 5 6 7 8
+	do
+		animal=`head -n $i $filename | tail -n 1`
+		file=$animal".jpg"
+		convert result.jpg $file -append result.jpg
+	done
+	eog -n result.jpg &
+}
+
+function namesort() #function that sorts the animals alphabeticaly
+{
+	FILES=$ANIMALS
+	for i in 1 2 3 4 5 6 7 8
+	do
+		NAME=`echo $FILES | cut -d' ' -f1`
+		FILES="`echo $FILES | cut -d' ' -f2-`"
+		echo $NAME >> names.txt
+	done
+	sort names.txt > /dev/null
+	cat names.txt
+}
+
+function wheightsort() #function that sorts the animals by wheight
+{
+	FILES=$ANIMALS
+	for i in 1 2 3 4 5 6 7 8
+	do
+		NAME=`echo $FILES | cut -d' ' -f1`
+		FILES="`echo $FILES | cut -d' ' -f2-`"
+		VAL=`cat $NAME".dat" | cut -d' ' -f1`
+		echo "$VAL $NAME " >> tmp.txt
+	done
+	cat tmp.txt | sort -n | cut -d' ' -f2 > wheight.txt
+	rm -f tmp.txt
+	cat wheight.txt
+}
+
+function lengthsort() #function that sorts the animals by length
+{
+	FILES=$ANIMALS
+	for i in 1 2 3 4 5 6 7 8
+	do
+		NAME=`echo $FILES | cut -d' ' -f1`
+		FILES="`echo $FILES | cut -d' ' -f2-`"
+		VAL=`cat $NAME".dat" | cut -d' ' -f2`
+		echo "$VAL $NAME " >> tmp.txt
+	done
+	cat tmp.txt | sort -n | cut -d' ' -f2 > length.txt
+	rm -f tmp.txt
+	cat length.txt
+}
+
+function main() #main function
+{
+	case "$1" in
+		alpha)
+			namesort
+			picmake names.txt
+			rm -f names.txt
+			;;
+		wheight)
+			wheightsort
+			picmake wheight.txt
+			rm -f wheight.txt
+			;;
+		length)
+			lengthsort
+			picmake length.txt
+			rm -f length.txt
+			;;
+		*)
+			echo -e "Insert one of the following parameters:\n	alpha (for alphabetical sorting);\n	wheight (for wheight sorting);\n	length (for length sorting)."
+	esac
+}
+
+main $1 #run the program
